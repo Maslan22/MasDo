0d9e7d77740bb8e05c1145018a006ad51e630841
HENRY IDDRISU MASLAN <henry.maslan@innorik.com>
2024-06-28 14:04:14 +0000

    convex setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
-import logo from "../public/logo.png";
 import { Button } from "@/components/ui/button";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
+import Tasks from "@/components/masdo/tasks";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Todovex</h1>
       <Button>Click me</Button>
-      {tasks?.map((task, idx)=> (
-        <p key={idx}>{JSON.stringify(task)}</p>
-      ))}
+      <Tasks />
     </main>
   );
 }
---
+++ components/masdo/tasks.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+export default function Tasks() {
+  const tasks = useQuery(api.tasks.get);
+  return (
+    <div>
+      <p>Task</p>
+      {tasks?.map((task, idx)=> (
+        <p key={idx}>{JSON.stringify(task)}</p>
+      ))}
+    </div>
+  );
+}
